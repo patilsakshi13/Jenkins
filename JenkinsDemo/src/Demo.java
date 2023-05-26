6cade6e6489b8895944c4e76e8a8577d63fb0b5b
patilsakshi13 <126866945+patilsakshi13@users.noreply.github.com>
2023-05-26 12:15:48 +0530

    Update Demo.java

--- JenkinsDemo/src/Demo.java
+++ JenkinsDemo/src/Demo.java
@@ -1,6 +1,12 @@
 
 public class Demo {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
+	System.out.println("hello");
+	System.out.println("hello");
+	System.out.println("hello");
+	System.out.println("hello");
+	System.out.println("hello");
 	System.out.println("hello");
 }
 }
